5bc97cea248de8440be61626c603058ec170c5b6
Dennis de Jong <dennisdejong1995@gmail.com>
2020-10-19 13:04:55 +0200

    Added checks for arguments to networkupwithcouchdb.sh

--- mount/network/scripts/networkupwithcouchdb.sh
+++ mount/network/scripts/networkupwithcouchdb.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-pushd ~/mount/network/docker
+if [[ $# -ne 1 ]] ; then
+    echo 'Please supply a path to the network'
+    exit 1
+fi
+
+HLF_PATH=$1
+
+pushd "$HLF_PATH"/mount/network/docker || exit
 
 docker-compose -f ./docker-compose-test-net.yaml -f ./docker-compose-couch.yaml up -d 
 
-popd
+popd || exit
