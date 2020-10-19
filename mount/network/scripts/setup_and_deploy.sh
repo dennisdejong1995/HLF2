6cc8492a724a949ee8714a9e424635d8166d9058
Dennis de Jong <dennisdejong1995@gmail.com>
2020-10-19 13:38:11 +0200

    Fixed channel create issue

--- mount/network/scripts/setup_and_deploy.sh
+++ mount/network/scripts/setup_and_deploy.sh
@@ -14,7 +14,9 @@ sudo ./generatecryptos.sh "$HLF_PATH"
 
 echo "Putting network up"
 sudo ./networkupwithcouchdb.sh "$HLF_PATH"
-sleep 10
+
+echo "Press return to continue"
+read -r
 
 echo "Creating channel"
 sudo ./createchannel.sh "$HLF_PATH"
