22cd6724581e85c0454773f1e136e177facdf2a8
Dennis de Jong <dennisdejong1995@gmail.com>
2020-10-22 14:45:30 +0200

    Fixed command issue builder

--- mount/network/scripts/comm_issue_builder_org1.sh
+++ mount/network/scripts/comm_issue_builder_org1.sh
@@ -10,4 +10,5 @@ paperNumber=$2
 issueDate=$3
 maturityDate=$4
 amount=$5
+echo ./comm_command_org1.sh -i -p "$HLF_PATH" -c "$base""$issuer""$split""$paperNumber""$split""$issueDate""$split""$maturityDate""$split""$amount""$end"
 sudo ./comm_command_org1.sh -i -p "$HLF_PATH" -c "$base""$issuer""$split""$paperNumber""$split""$issueDate""$split""$maturityDate""$split""$amount""$end"
