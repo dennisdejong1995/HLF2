86bb6547d8c6ce8e6c72e428ee7361b0a701ae28
Dennis de Jong <dennisdejong1995@gmail.com>
2020-10-28 12:24:15 +0100

    Test

--- mount/chaincode/commercial-paper/go/commercial-paper/papercontract.go
+++ mount/chaincode/commercial-paper/go/commercial-paper/papercontract.go
@@ -106,13 +106,3 @@ func (c *Contract) GetOne(ctx TransactionContextInterface, issuer string, paperN
 
 	return paper, nil
 }
-func (c *Contract) GetState(ctx TransactionContextInterface, issuer string, paperNumber string) (*State, error) {
-	paper, err := ctx.GetPaperList().GetPaper(issuer, paperNumber)
-
-	if err != nil {
-		return nil, err
-	}
-	state := paper.GetState()
-
-	return &state, nil
-}
