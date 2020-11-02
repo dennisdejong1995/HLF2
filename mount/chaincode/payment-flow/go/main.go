9fb58463dc03054b595bf4762a37248bb01bd8fe
Dennis de Jong <dennisdejong1995@gmail.com>
2020-11-02 11:16:15 +0100

    Added go mod

--- mount/chaincode/payment-flow/go/main.go
+++ mount/chaincode/payment-flow/go/main.go
@@ -10,7 +10,7 @@ func main() {
 
 	contract := new(payment_flow.Contract)
 	contract.TransactionContextHandler = new(payment_flow.TransactionContext)
-	contract.Name = "org.dealblock.erc721"
+	contract.Name = "org.dealblock.payment_flow"
 	contract.Info.Version = "0.0.1"
 
 	chaincode, err := contractapi.NewChaincode(contract)
@@ -19,7 +19,7 @@ func main() {
 		panic(fmt.Sprintf("Error creating chaincode. %s", err.Error()))
 	}
 
-	chaincode.Info.Title = "ERC721Chaincode"
+	chaincode.Info.Title = "PaymentFlowChaincode"
 	chaincode.Info.Version = "0.0.1"
 
 	err = chaincode.Start()
