c48cbfae316a9e921e0a6408f5cad1ee0e3f9352
Dennis de Jong <dennisdejong1995@gmail.com>
2020-11-30 09:42:25 +0100

    Added API connection

--- mount/chaincode/payment-flow/go/payment-flow/currency_exchange.go
+++ mount/chaincode/payment-flow/go/payment-flow/currency_exchange.go
@@ -1,10 +1,35 @@
 package payment_flow
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func ExchangeUSDT(amount int, receiver string, receiverAddress string, sender string, senderAddress string) (string, error) {
 	// TODO: Add API connection to Ethereum for exchanging tether
 	fmt.Printf("Exchanging %d in USDT to %s from %s\n", amount, receiver, sender)
+
+	url := "https://mainnet.infura.io/v3/189920b69bd147cbbee96ca2c36e5ea3"
+	fmt.Println("URL:>", url)
+
+	var jsonStr = []byte(`{"jsonrpc":"2.0","method":"eth_blockNumber","params": [],"id":1}`)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+
 	return "example_hash", nil
 }
 
